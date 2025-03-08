34d8a00afc41982cacd992303754a0d6f2d06a9a
KimGiii <gibo9809@gmail.com>
2025-03-08 23:08:58 +0900

    DIP 적용 - input handler, outhandler 분리

--- src/main/java/cleancode/minesweeper/tobe/GameApplication.java
+++ src/main/java/cleancode/minesweeper/tobe/GameApplication.java
@@ -1,15 +1,31 @@
 package cleancode.minesweeper.tobe;
 
 import cleancode.minesweeper.tobe.gamelevel.GameLevel;
-import cleancode.minesweeper.tobe.gamelevel.Middle;
+import cleancode.minesweeper.tobe.gamelevel.Begginer;
+import cleancode.minesweeper.tobe.io.ConsoleInputHandler;
+import cleancode.minesweeper.tobe.io.ConsoleOutputHandler;
+import cleancode.minesweeper.tobe.io.InputHandler;
+import cleancode.minesweeper.tobe.io.OutputHandler;
 
 public class GameApplication {
 
     public static void main(String[] args) {
-        GameLevel gameLevel = new Middle();
+        GameLevel gameLevel = new Begginer();
+        InputHandler inputHandler = new ConsoleInputHandler();
+        OutputHandler outputHandler = new ConsoleOutputHandler();
 
-        Minesweeper minesweeper = new Minesweeper(gameLevel);
+        Minesweeper minesweeper = new Minesweeper(gameLevel, inputHandler, outputHandler);
+        minesweeper.initialize();
         minesweeper.run();
     }
 
+
+    /**
+     * DIP - Dependency Inversion Principle
+     *
+     * DI - Dependency Injection -> "3"
+     *
+     * IoC - Inversion of Control
+     * 프로그램의 흐름을 framework에 의해 제어된다.
+     */
 }

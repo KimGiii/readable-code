291fd89e341299775d09798dd6ad94534b9f2261
KimGiii <gibo9809@gmail.com>
2025-03-12 12:35:14 +0900

    패키지 정리

---
+++ src/main/java/cleancode/minesweeper/tobe/minesweeper/board/cell/Cell.java
@@ -0,0 +1,27 @@
+package cleancode.minesweeper.tobe.minesweeper.board.cell;
+
+/*
+    도메인 지식을 얻었따!
+
+    1. 열림/닫힘 개념과 사용자가 확인했다 개념은 다름
+    2. sign(String) 기반의 BOARD가 있고, 상황에 따라 표시할 sign을 갈아끼움
+       -> Cell이라는 정보를 담을 공간, 객체가 생김
+       -> board는 Cell의 상태를 변화시키는 방향으로 가야함!
+ */
+
+public interface Cell {
+
+    boolean isLandMine();
+
+    boolean hasLandMineCount();
+
+    CellSnapshot getSnapshot();
+
+    void flag();
+
+    void open();
+
+    boolean isChecked();
+
+    boolean isOpened();
+}

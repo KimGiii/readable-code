e411785336d5b5cf75c9585ce104269bfcb37972
KimGiii <gibo9809@gmail.com>
2025-03-17 23:00:50 +0900

    isSameDurationType 단위 테스트 수정

--- src/test/java/cleancode/studycafe/tobe/model/pass/StudyCafeSeatPassTest.java
+++ src/test/java/cleancode/studycafe/tobe/model/pass/StudyCafeSeatPassTest.java
@@ -1,9 +1,11 @@
 package cleancode.studycafe.tobe.model.pass;
 
+import cleancode.studycafe.tobe.model.pass.locker.StudyCafeLockerPass;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static cleancode.studycafe.tobe.model.pass.StudyCafePassType.HOURLY;
+import static cleancode.studycafe.tobe.model.pass.StudyCafePassType.*;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 class StudyCafeSeatPassTest {
@@ -16,8 +18,22 @@ class StudyCafeSeatPassTest {
 
         // when
         int price = seatPass.getPrice();
+
         // then
-        assertEquals(4000, price);
+        assertThat(price).isEqualTo(4000);
     }
-  
+
+    @DisplayName("사용자가 선택한 사물함 이용권의 기간과 가격이 맞는지 확인한다.")
+    @Test
+    void isSameDurationType() {
+        // given
+        StudyCafeLockerPass lockerPass = StudyCafeLockerPass.of(FIXED, 4, 10000);
+
+        // when
+        boolean lockerPassDuration = lockerPass.isSameDuration(4);
+        boolean lockerPassType = lockerPass.isSamePassType(FIXED);
+
+        // then
+        assertThat(lockerPassType && lockerPassDuration).isTrue();
+    }
 }
